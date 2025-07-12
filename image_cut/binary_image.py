c105fcbef8f1f047d7fd6a878787bd2c0d72a8cd
David-wang711 <124090664@link.cuhk.edu.cn>
2025-07-12 12:49:31 +0800

    Update binary_image.py

--- image_cut/binary_image.py
+++ image_cut/binary_image.py
@@ -2,8 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 def resize(img:str):#resize a image into 128*128 resolution
-
-    img = cv2.imread(img,cv2.IMREAD_GRAYSCALE)
+    
     ratio = img.shape[1]/img.shape[0]
     size = (128,int(128/ratio))
     sigma_x,sigma_y = int(img.shape[0]/100),int(img.shape[1]/100)
